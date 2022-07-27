d7c04f5e4fa2ba3acdb28b4b8363a4cebede01ea
theresia0168 <88471313+theresia0168@users.noreply.github.com>
2022-07-27 16:00:50 +0900

    Update and rename p4.c to p04.c

--- p04.c
+++ p04.c
@@ -1,7 +1,5 @@
 #include<stdio.h>
 
-
-
 int main(int argc, char *argv[])
 {
 	int x=15;
@@ -10,7 +8,7 @@ int main(int argc, char *argv[])
 	x*=x;
 	x+=7;
 	
-	printf("xÀÇ ÃÖÁ¾ °ª : %d, %d, %.1f\n", x, y, z);
+	printf("xÃ€Ã‡ ÃƒÃ–ÃÂ¾ Â°Âª : %d, %d, %.1f\n", x, y, z);
 	
 	return 0;
 }
