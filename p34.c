990d5593f77ab54e37858d808893b3c330465676
AhnChaeHyeok <88471313+theresia0168@users.noreply.github.com>
2022-07-29 15:40:56 +0900

    Update p34.c

--- p34.c
+++ p34.c
@@ -5,16 +5,14 @@ int main(int argc, char *argv[]){
 	
 	time_t ts;
 	struct tm *busan_time;
-	char *mon_names[]={"JAN","FEB","MAR","APR","MAY","JUN",
-						"JUL","AUG","SEP","OCT","NOV","DEC"};
+	char *mon_names[]={"JAN","FEB","MAR","APR","MAY","JUN","JUL","AUG","SEP","OCT","NOV","DEC"};
 	
 	ts =time(NULL);
 	//printf("%d\n", ts);
 	
 	busan_time = localtime(&ts);
 	printf("%d\n",busan_time->tm_year+1900);
-	printf("%d, %s\n", busan_time->tm_mon+1,
-						mon_names[busan_time->tm_mon]);
+	printf("%d, %s\n", busan_time->tm_mon+1, mon_names[busan_time->tm_mon]);
 	printf("%d\n", busan_time->tm_mday);
 	
 	return 0;
